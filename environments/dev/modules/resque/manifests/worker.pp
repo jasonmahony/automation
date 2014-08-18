5e3b8950035b7059fb1905bcbd189d27e737d6f3
Jason Mahony <jason@verticalresponse.com>
2014-08-18 00:12:35 -0700

    minor worker.pp improvement

--- environments/dev/modules/resque/manifests/worker.pp
+++ environments/dev/modules/resque/manifests/worker.pp
@@ -1,4 +1,4 @@
 class resque::worker inherits resque {
   File { mode => 644, ensure => present, content => template('resque/resque-generic.erb') }
-  file { '/etc/init/resque-${resque_queue}.conf': }
+  file { '/etc/init/resque-${name}.conf': }
 }
