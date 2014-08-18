dc2a1eedd5e09d21d58e4a5df1591a0a33818e3e
Jason Mahony <jason@verticalresponse.com>
2014-08-18 00:07:58 -0700

    added resque module

---
+++ environments/dev/modules/resque/manifests/init.pp
@@ -0,0 +1,17 @@
+class resque {
+
+  $rails_env = "dev"
+
+  file { '/etc/init.d/resque': mode => 755, ensure => present, source => "puppet:///modules/resque/resque-init" }
+  file { '/etc/init/resque-scheduler.conf': mode => 0644, ensure => present, source => "puppet:///modules/resque/resque-scheduler.conf" }
+
+
+  resque::worker { 'event': resque_queue => "event" }
+  resque::worker { 'signin': resque_queue => "signin" }
+
+  package { 'resque':
+    ensure   => 'installed',
+    provider => 'gem',
+  }
+
+}
---
+++ environments/dev/modules/resque/manifests/worker.pp
@@ -0,0 +1,4 @@
+class resque::worker inherits resque {
+  File { mode => 644, ensure => present, content => template('resque/resque-generic.erb') }
+  file { '/etc/init/resque-${resque_queue}.conf': }
+}
