c058c37c8b58356c6baab5d44ba260164b053111
Jason Mahony <jason@verticalresponse.com>
2014-11-10 22:10:38 -0800

    postgres dev module allow_virtual false

--- environments/dev/modules/postgres/manifests/install.pp
+++ environments/dev/modules/postgres/manifests/install.pp
@@ -1,7 +1,8 @@
 class postgres::install {
 	
 	File { ensure => directory, owner => root, group => root, mode => 0755 }
-
+  $source = "puppet:///modules/postgres"
+	
 	# set up pgsql directories (puppet won't create intermediate dirs)
 	file { '/db': }
 	file { '/db/pgsql': require => File['/db'] }
@@ -10,7 +11,7 @@ class postgres::install {
   file { '/db/log/pg_xlog': owner => postgres, group => postgres, require => File['/db/log'] }
 
 	unless $pgsql_version { notify {'init_message': message => 'postgres database not yet initialized; not placing config files.' } }
-  package { "$::postgres::packages": ensure => installed, allow_virtual => true }
+  package { "$::postgres::packages": ensure => installed, allow_virtual => false }
 #
 #	package { "$::postgres::packages": ensure => installed, allow_virtual => false }
 # 
