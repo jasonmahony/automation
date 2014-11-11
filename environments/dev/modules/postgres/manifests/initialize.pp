a0ae9cffaeac93ec4fc08a4ee255863cb18a100d
Jason Mahony <jason@verticalresponse.com>
2014-11-11 00:59:06 -0800

    more env variables

--- environments/dev/modules/postgres/manifests/initialize.pp
+++ environments/dev/modules/postgres/manifests/initialize.pp
@@ -1,11 +1,11 @@
 class postgres::initialize {
   include postgres::params
-  $postgres_init = $postgres::params::postgres_init
+  $postgres_service = $postgres::params::postgres_service
 
   unless $pgsql_version { 
     notify { 'init_message': message => 'postgres database not yet initialized; not placing config files until initialized.' }
     exec { "initialize_database": 
-      command => "$postgres_init initdb -X /db/log/pg_xlog",
+      command => "/sbin/service $postgres_service initdb -X /db/log/pg_xlog",
       require => Exec["delete_default_configs"]
     }
     exec { "delete_default_configs": command => "/bin/rm -f /db/pgsql/data/{postgresql.conf,pg_hba.conf}" }
