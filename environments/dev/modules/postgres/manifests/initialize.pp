5ce2bae17d6791af2d458b167127584780cdfbd3
Jason Mahony <jason@verticalresponse.com>
2014-11-10 22:47:15 -0800

    postgres dev module

--- environments/dev/modules/postgres/manifests/initialize.pp
+++ environments/dev/modules/postgres/manifests/initialize.pp
@@ -8,9 +8,6 @@ class postgres::initialize {
       command => "/sbin/service $postgres_version initdb -X /db/log/pg_xlog",
       require => Exec["delete_default_configs"]
     }
-    exec { "delete_default_configs":
-      command => "/bin/rm -f /db/pgsql/data/{postgresql.conf,pg_hba.conf}",
-      require => Package["${postgres_version}-server"]
-    }
+    exec { "delete_default_configs": command => "/bin/rm -f /db/pgsql/data/{postgresql.conf,pg_hba.conf}" }
   }
 }
