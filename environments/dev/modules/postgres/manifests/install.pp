95c6f7b6c8161e18a1e11924ed4af25f0f73661b
Jason Mahony <jason@verticalresponse.com>
2014-11-10 23:11:47 -0800

    postgres dev module

--- environments/dev/modules/postgres/manifests/install.pp
+++ environments/dev/modules/postgres/manifests/install.pp
@@ -9,15 +9,15 @@ class postgres::install {
 	# set up pgsql directories (puppet won't create intermediate dirs)
 	file { '/db': }
 	file { '/db/pgsql': require => File['/db'] }
-	file { '/db/pgsql/data': mode => 0700, owner => postgres, group => postgres, require => File['/db/pgsql'] }
-	file { '/db/log': owner => postgres, group => postgres, require => File['/db'] }
-  file { '/db/log/pg_xlog': owner => postgres, group => postgres, require => File['/db/log'] }
+	file { '/db/pgsql/data': mode => 0700, owner => postgres, group => postgres, require => File['/db/pgsql'], require => Package['$postgres_version'] }
+	file { '/db/log': owner => postgres, group => postgres, require => File['/db'], require => Package['$postgres_version'] }
+  file { '/db/log/pg_xlog': owner => postgres, group => postgres, require => File['/db/log'] , require => Package['$postgres_version']}
 
   package { "$postgres_version": }
-#  package { '$postgres_version-server': }
-#  package { '$postgres_version-lib': }
-#  package { '$postgres_version-contrib': }
-#  package { '$postgres_version-devel': }
+  package { '$postgres_version-server': }
+  package { '$postgres_version-lib': }
+  package { '$postgres_version-contrib': }
+  package { '$postgres_version-devel': }
 
   file { "/etc/sysconfig/pgsql/$postgres_version": mode => 0644, source => "$source/conf" }
 }
