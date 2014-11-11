239f8ee623efe81bf8eb779f3aed8e3b849e3c0b
Jason Mahony <jason@verticalresponse.com>
2014-11-10 18:45:46 -0800

    typo in resque template for dev

--- environments/dev/modules/resque/manifests/init.pp
+++ environments/dev/modules/resque/manifests/init.pp
@@ -5,7 +5,6 @@ class resque {
   file { '/etc/init.d/resque': mode => 755, ensure => present, source => "puppet:///modules/resque/resque-init" }
   file { '/etc/init/resque-scheduler.conf': mode => 0644, ensure => present, source => "puppet:///modules/resque/resque-scheduler.conf" }
 
-
   resque::worker { 'event': resque_queue => "event" }
   resque::worker { 'signin': resque_queue => "signin" }
 
--- environments/production/modules/resque/manifests/init.pp
+++ environments/production/modules/resque/manifests/init.pp
@@ -1,17 +1,13 @@
-class resque {
+class resque inherits resque::worker {
 
   $rails_env = "production"
 
   file { '/etc/init.d/resque': mode => 755, ensure => present, source => "puppet:///modules/resque/resque-init" }
   file { '/etc/init/resque-scheduler.conf': mode => 0644, ensure => present, source => "puppet:///modules/resque/resque-scheduler.conf" }
 
-
   resque::worker { 'event': resque_queue => "event" }
   resque::worker { 'signin': resque_queue => "signin" }
 
-  package { 'resque':
-    ensure   => 'installed',
-    provider => 'gem',
-  }
+  package { 'resque': ensure => 'installed', provider => 'gem' }
 
 }
