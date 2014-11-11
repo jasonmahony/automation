ac6d4df2ad56bd024c05efa240cadb7329936ef7
Jason Mahony <jason@verticalresponse.com>
2014-11-10 23:28:51 -0800

    postgres dev module

--- environments/dev/modules/postgres/manifests/initialize.pp
+++ environments/dev/modules/postgres/manifests/initialize.pp
@@ -5,7 +5,7 @@ class postgres::initialize {
   unless $pgsql_version { 
     notify { 'init_message': message => 'postgres database not yet initialized; not placing config files until after initialization.' }
     exec { "initialize_database": 
-      command => "/sbin/service $postgres_version initdb -X /db/log/pg_xlog",
+      command => "/etc/init.d/$postgres_version initdb -X /db/log/pg_xlog",
       require => Exec["delete_default_configs"]
     }
     exec { "delete_default_configs": command => "/bin/rm -f /db/pgsql/data/{postgresql.conf,pg_hba.conf}" }
