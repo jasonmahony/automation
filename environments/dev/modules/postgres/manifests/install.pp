6ef2ab533302f9e9e4fba4c7f2eb0a3ac59dafed
Jason Mahony <jason@verticalresponse.com>
2014-11-10 21:55:56 -0800

    postgres dev module

--- environments/dev/modules/postgres/manifests/install.pp
+++ environments/dev/modules/postgres/manifests/install.pp
@@ -9,7 +9,7 @@ class postgres::install {
 	file { '/db/log': owner => postgres, group => postgres, require => File['/db'] }
   file { '/db/log/pg_xlog': owner => postgres, group => postgres, require => File['/db/log'] }
 
-	unless $pgsql_version { notify { 'postgres database not yet initialized; not placing config files.' } }
+	unless $pgsql_version { notify {'init_message': message => 'postgres database not yet initialized; not placing config files.' } }
   package { "$::postgres::packages": ensure => installed, allow_virtual => true }
 #
 #	package { "$::postgres::packages": ensure => installed, allow_virtual => false }
