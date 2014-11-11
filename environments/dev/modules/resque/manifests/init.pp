096402b7fb4786d50ea3995bc405b789299336c5
Jason Mahony <jason@verticalresponse.com>
2014-11-10 16:41:46 -0800

    cleaning up resque and postgres modules

--- environments/dev/modules/resque/manifests/init.pp
+++ environments/dev/modules/resque/manifests/init.pp
@@ -1,6 +1,6 @@
 class resque {
 
-  $rails_env = "dev"
+#  $rails_env = "dev"
 
   file { '/etc/init.d/resque': mode => 755, ensure => present, source => "puppet:///modules/resque/resque-init" }
   file { '/etc/init/resque-scheduler.conf': mode => 0644, ensure => present, source => "puppet:///modules/resque/resque-scheduler.conf" }
@@ -9,9 +9,6 @@ class resque {
   resque::worker { 'event': resque_queue => "event" }
   resque::worker { 'signin': resque_queue => "signin" }
 
-  package { 'resque':
-    ensure   => 'installed',
-    provider => 'gem',
-  }
+  package { 'resque': ensure => 'installed', provider => 'gem' }
 
 }
