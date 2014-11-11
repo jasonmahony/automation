f77f1c315d39dca27827acaa014dc450da2d2dc3
Jason Mahony <jason@verticalresponse.com>
2014-11-10 21:43:23 -0800

    resque for dev

--- environments/dev/modules/resque/manifests/init.pp
+++ environments/dev/modules/resque/manifests/init.pp
@@ -1,13 +1,18 @@
 class resque {
 
-#  $rails_env = "dev"
+  define worker ( $resque_queue, $rails_env ) {
+     file { "/etc/init/${title}.conf":
+     mode    => 644, 
+     ensure  => present, 
+     content => template('resque/resque-generic.conf.erb') 
+     }
+  }
 
+  worker { 'resque-event': resque_queue => "event", rails_env => "dev" }
+  worker { 'resque-signin': resque_queue => "signin", rails_env => "dev" }
+    
   file { '/etc/init.d/resque': mode => 755, ensure => present, source => "puppet:///modules/resque/resque-init" }
   file { '/etc/init/resque-scheduler.conf': mode => 0644, ensure => present, source => "puppet:///modules/resque/resque-scheduler.conf" }
-
-  resque::worker { 'event': resque_queue => "event" }
-  resque::worker { 'signin': resque_queue => "signin" }
-
   package { 'resque': ensure => 'installed', provider => 'gem' }
 
 }
