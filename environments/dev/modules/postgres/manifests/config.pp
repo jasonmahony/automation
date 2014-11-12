fa835fc40f6739476adfae98c86563ce5e518c2c
Jason Mahony <jason@verticalresponse.com>
2014-11-11 19:26:52 -0800

    testing symlink

--- environments/dev/modules/postgres/manifests/config.pp
+++ environments/dev/modules/postgres/manifests/config.pp
@@ -7,6 +7,6 @@ class postgres::config {
   file { "/db/pgsql/data/postgresql.conf": content => template('postgres/postgresconf.erb') }
   # postgres client config file
   file { "/db/pgsql/data/pg_hba.conf": source => "$source/pg_hba.conf" }
-#  file { "/db/pgsql/data/pg_xlog": ensure => 'link', force => true, target => '/db/log/pg_xlog' }
+  file { "/db/pgsql/data/pg_xlog": ensure => 'link', force => true, target => '/db/log/pg_xlog' }
 
 }
