057f04ec01be876bbcee0042cc2d98dc2627905f
Jason Mahony <jason@verticalresponse.com>
2014-11-11 18:22:25 -0800

    force is true in config postgres dev

--- environments/dev/modules/postgres/manifests/config.pp
+++ environments/dev/modules/postgres/manifests/config.pp
@@ -4,8 +4,8 @@ class postgres::config {
 	$source = "puppet:///modules/postgres"
 	
   # postgres server config file
-  file { "/db/pgsql/data/postgresql.conf": content => template('postgres/postgresconf.erb') }		
+  file { "/db/pgsql/data/postgresql.conf": content => template('postgres/postgresconf.erb'), force => true }		
   # postgres client config file
-  file { "/db/pgsql/data/pg_hba.conf": source => "$source/pg_hba.conf" }   
+  file { "/db/pgsql/data/pg_hba.conf": source => "$source/pg_hba.conf", force => true }   
   file { "/db/pgsql/data/pg_xlog": ensure => 'link', force => true, target => '/db/log/pg_xlog' }
 }
