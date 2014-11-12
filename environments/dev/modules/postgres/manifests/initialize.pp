06dc65e0ad13485b2417703a61168d61464c84bd
Jason Mahony <jason@verticalresponse.com>
2014-11-11 18:15:10 -0800

    typo in initialize class

--- environments/dev/modules/postgres/manifests/initialize.pp
+++ environments/dev/modules/postgres/manifests/initialize.pp
@@ -4,9 +4,10 @@ class postgres::initialize {
 
   unless $pgsql_version { 
     notify { 'init_message': message => 'postgres database not yet initialized; not placing config files until initialized.' }
-    exec { 'initialize_database': command => "/sbin/service ${service} initdb -X /db/log/pg_xlog", require => Exec["delete_default_congifs"] }
-    exec { "delete_default_configs":
-      command => "/bin/rm -f /db/pgsql/data/{postgresql.conf,pg_hba.conf}"
+    exec { 'initialize_database':
+      command => "/sbin/service ${service} initdb -X /db/log/pg_xlog", 
+      require => Exec["delete_default_configs"]
     }
+    exec { "delete_default_configs": command => "/bin/rm -f /db/pgsql/data/{postgresql.conf,pg_hba.conf}" }
   }
 }
