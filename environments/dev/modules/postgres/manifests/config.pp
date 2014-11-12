0d04e4565bee7c990212708a52fb195aa87015ef
Jason Mahony <jason@verticalresponse.com>
2014-11-11 18:35:21 -0800

    backup is true in config postgres dev

--- environments/dev/modules/postgres/manifests/config.pp
+++ environments/dev/modules/postgres/manifests/config.pp
@@ -4,8 +4,9 @@ class postgres::config {
 	$source = "puppet:///modules/postgres"
 	
   # postgres server config file
-  file { "/db/pgsql/data/postgresql.conf": content => template('postgres/postgresconf.erb'), force => true }		
+  file { "/db/pgsql/data/postgresql.conf": content => template('postgres/postgresconf.erb'), backup => ".puppetbak" }
   # postgres client config file
-  file { "/db/pgsql/data/pg_hba.conf": source => "$source/pg_hba.conf", force => true }   
+  file { "/db/pgsql/data/pg_hba.conf": source => "$source/pg_hba.conf", backup => ".puppetbak" }
   file { "/db/pgsql/data/pg_xlog": ensure => 'link', force => true, target => '/db/log/pg_xlog' }
+
 }
