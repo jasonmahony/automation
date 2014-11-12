d156a2bb0bda6ee3fbae226c293c6da832a95e86
Jason Mahony <jason@verticalresponse.com>
2014-11-11 17:46:38 -0800

    postgres dev module require fix

--- environments/dev/modules/postgres/manifests/install.pp
+++ environments/dev/modules/postgres/manifests/install.pp
@@ -4,7 +4,7 @@ class postgres::install {
 	$postgres_conf = $postgres::params::postgres_conf
   $postgres_init = $postgres::params::postgres_init
 	
-	File { ensure => directory, owner => root, group => root, mode => 0755, require => Package["$postgres_version"] }
+	File { ensure => directory, owner => root, group => root, mode => 0755, require => Package["$postgres_version-server"] }
   Package { ensure => installed, allow_virtual => true }
   $source = "puppet:///modules/postgres"
 	
@@ -17,9 +17,9 @@ class postgres::install {
 	# set up pgsql directories (puppet won't create intermediate dirs)
 	file { '/db': }
 	file { '/db/pgsql': require => File['/db'] }
-	file { '/db/pgsql/data': mode => 0700, owner => postgres, group => postgres, require => File['/db/pgsql'] }
-	file { '/db/log': owner => postgres, group => postgres, require => File['/db'] }
-  file { '/db/log/pg_xlog': owner => postgres, group => postgres, require => File['/db/log'] }
-  file { "$postgres_conf": ensure => present, mode => 0644, source => "$source/conf", require => File["/db"] }
-  file { "$postgres_init": ensure => present, source => "$source/postgres_init", require => File["/db"] }
+	file { '/db/pgsql/data': mode => 0700, owner => postgres, group => postgres }
+	file { '/db/log': owner => postgres, group => postgres }
+  file { '/db/log/pg_xlog': owner => postgres, group => postgres }
+  file { "$postgres_conf": ensure => present, mode => 0644, source => "$source/conf" }
+  file { "$postgres_init": ensure => present, source => "$source/postgres_init" }
 }
